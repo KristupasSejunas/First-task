a80f72654922c7f1b199ad1f50b4309664746f6d
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-03-05 15:41:35 +0200

    v0.3

---
+++ v0.3/Rusiavimas.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "data.h"
+
+bool pagal_varda(data a, data b);
+bool pagal_pavarde(data a, data b);
---
+++ v0.3/Skaiciavimai.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "data.h"
+
+double Vidurkis(vector<int> paz);
+double Mediana(vector<int> paz);
+void skaiciavimas(vector<data>& sarasas, int arVM);
