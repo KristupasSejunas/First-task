f9f8d17f515fa222b95a75f72da3ff42a02a75ee
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-03-20 22:37:20 +0200

    Delete v0.3 directory

--- v0.3/Generavimas.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "data.h"
-
-int skaicius();
-void random_paz(int k, data& temp);
--- v0.3/Skaiciavimai.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "data.h"
-
-double Vidurkis(vector<int> paz);
-double Mediana(vector<int> paz);
-void skaiciavimas(vector<data>& sarasas, int arVM);
