9e33da20533a1fcec6f00c044a7c03c916ab5c18
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-04-03 19:35:38 +0300

    deque for speed testing

---
+++ v0.5_deque/Rusiavimas.cpp
@@ -0,0 +1,30 @@
+#include "Rusiavimas.h"
+
+bool pagal_varda(data a, data b)
+{
+	return a.vardas < b.vardas;
+}
+bool pagal_pavarde(data a, data b)
+{
+	return a.pavarde < b.pavarde;
+}
+void studentu_skirtymas(deque<data>& sarasas, deque<data>& pirmunai, deque<data>& nuskriaustieji)
+{
+	auto x = std::chrono::high_resolution_clock::now();
+	int i = 1;
+	for (auto& s : sarasas)
+	{
+		if (s.result < 5)
+		{
+			nuskriaustieji.push_back(s);
+		}
+		else
+		{
+			pirmunai.push_back(s);
+		}
+		i++;
+	}
+	sarasas.clear();	
+	std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - x;
+	cout << "Studentu dalinimas uztruko: " << diff.count() << " s" << endl;
+}
