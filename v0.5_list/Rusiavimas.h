053ebec3bf47dd45b1a3f34ed60d514f11684d00
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-04-03 20:57:50 +0300

    list for speed testing

---
+++ v0.5_list/Rusiavimas.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "data.h"
+
+bool pagal_varda(data a, data b);
+bool pagal_pavarde(data a, data b);
+void studentu_skirtymas(list<data>& sarasas, list<data>& pirmunai, list<data>& nuskriaustieji);
