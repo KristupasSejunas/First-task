3c9919b02f0027f3de79a9baab1b838f8e04c4f6
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-04-17 20:26:23 +0300

    Add files via upload

---
+++ v1.0_2_strategija_vector/Rusiavimas.cpp
@@ -0,0 +1,34 @@
+#include "Rusiavimas.h"
+
+bool pagal_varda(data a, data b)
+{
+	return a.vardas < b.vardas;
+}
+bool pagal_pavarde(data a, data b)
+{
+	return a.pavarde < b.pavarde;
+}
+bool pagal_pazymi(data a, data b)
+{
+	return a.result > b.result;
+}
+void studentu_skirtymas(vector<data>& sarasas, vector<data>& pirmunai, vector<data>& nuskriaustieji)
+{
+	sort(sarasas.begin(), sarasas.end(), pagal_pazymi);
+	auto x = std::chrono::high_resolution_clock::now();
+	vector<data> ::iterator it = sarasas.end();
+	it--;
+	for (auto i = sarasas.size() - 1; i > 0; i--)
+	{
+		if (it->result < 5)
+		{
+			nuskriaustieji.push_back(*it);
+			sarasas.resize(sarasas.size() - 1);
+		}
+		it--;
+	}
+	pirmunai = sarasas;
+	sarasas.clear();
+	std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - x;
+	cout << "Studentu dalinimas uztruko: " << diff.count() << " s" << endl;
+}
