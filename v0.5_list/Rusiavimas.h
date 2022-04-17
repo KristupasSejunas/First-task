72b31c010e4d3db854f587a95ea0c3da696fe0ff
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-04-17 18:46:29 +0300

    Delete v0.5_list directory

--- v0.5_list/Rusiavimas.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "data.h"
-
-bool pagal_varda(data a, data b);
-bool pagal_pavarde(data a, data b);
-bool pagal_pazymi(data a, data b);
-void studentu_skirtymas(list<data>& sarasas, list<data>& pirmunai, list<data>& nuskriaustieji);
--- v0.5_list/Skaiciavimas.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "data.h"
-
-double Vidurkis(vector<int> paz);
-double Mediana(vector<int> paz);
-void skaiciavimas(list<data>& sarasas, int arVM);
