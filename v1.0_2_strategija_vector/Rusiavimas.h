2f106273b49d3819288c4c7dc7df686e060156b3
ugnemeskuotyte <45960501+ugnemeskuotyte@users.noreply.github.com>
2022-05-01 00:06:45 +0300

    Delete v1.0_2_strategija_vector directory

--- v1.0_2_strategija_vector/Rusiavimas.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "data.h"
-
-bool pagal_varda(data a, data b);
-bool pagal_pavarde(data a, data b);
-bool pagal_pazymi(data a, data b);
-void studentu_skirtymas(vector<data>& sarasas, vector<data>& pirmunai, vector<data>& nuskriaustieji);
